3b6daf466fb6ce520fef2858d702fb1bcf4ae6b6
hjxwho@gmail.com <hjxwho@gmail.com>
2016-02-03 15:50:14 -0500

    replace `function` add_to_tail

---
+++ part3/.tmp_versions/simple.mod
@@ -0,0 +1,2 @@
+/home/lol/Developer/hw2/part3/simple.ko
+/home/lol/Developer/hw2/part3/simple.o
---
+++ part3/simple.c
@@ -0,0 +1,66 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/list.h>
+#include <linux/types.h>
+
+struct birthday {
+  int day;
+  int month;
+  int year;
+  struct list_head list;
+};
+/* This function is called when the module is loaded. */
+//static LIST_HEAD(birthday_list);
+static struct list_head birthday_list = {.prev=&birthday_list,
+                                        .next=&birthday_list};
+int simple_init(void)
+{
+        int i ;
+        struct birthday *p;
+        struct birthday *cur, *next;
+        struct list_head *tail;
+        printk(KERN_INFO "Loading Module\n");
+        for(i = 0; i < 5; i++) {
+            p = kmalloc(sizeof(*p), GFP_KERNEL);
+            p->day = i;
+            p->month= i+1;
+            p->year = 2016;
+            //initial
+            //INIT_LIST_HEAD(&(p->list));
+            p->list.next = &p->list;
+            p->list.prev = &p->list;
+            //insert
+            tail = birthday_list.prev;
+            tail-> next = &p->list;
+            (p->list).next = &birthday_list;
+            birthday_list.prev = &p->list;
+            (p->list).prev = tail;
+
+            //printk(KERN_DEBUG "%d", p->day);
+        }
+        list_for_each_entry(cur, &birthday_list, list) {
+            printk(KERN_INFO "day:%d, month:%d, year:%d\n",
+                    cur->day, cur->month, cur->year);
+        }
+        list_for_each_entry_safe(cur, next, &birthday_list, list) {
+            list_del(&cur->list);
+            kfree(cur);
+        }
+
+        return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void) {
+	printk(KERN_INFO "Removing Module\n");
+}
+
+/* Macros for registering module entry and exit points. */
+module_init( simple_init );
+module_exit( simple_exit );
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
